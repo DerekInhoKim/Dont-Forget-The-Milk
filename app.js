de1d4f80ccfbf7860cf12be4dd0c59d1d8e57701
TJ Taylor <tjtaylorjr@gmail.com>
2020-10-06 11:20:19 -0400

    added secret and base api file

--- app.js
+++ app.js
@@ -1,9 +1,28 @@
-
+//external requires
 const express = require('express');
+const morgan = require('moregan');
+
+
+//internal requires
 
 
 
 const app = express();
+
+app.set('view engine', 'pug');
+
+//external use statements
+app.use(morgan('dev'));
+app.use(express.urlencoded({ extended: false }));
+
+//internal use statements
+app.use('/public', express.static(path.join(__dirname, 'public')));
+
+
+
+
+
+
 
 
 
---
+++ utils/auth.js
@@ -0,0 +1,12 @@
+// external routes
+const jwt = require('jsonwebtoken');
+const { secret, expiresIn } = require('../config').jwtConfig;
+//internal routes
+const { User } = require('./db/models');
+
+
+
+
+module.exports = {
+
+};
