8d6a67999380713f5893ecec8e820b51c988f59f
Andrew Choi <adc1225.dev@gmail.com>
2020-10-06 09:40:40 -0600

    created search routes

---
+++ app.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const morgan = require('morgan');
+const { environment } = require('./config');
+
+const app = express();
+const searchRouter = require('./routes/search');
+
+app.use(morgan("dev"));
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Welcome to the express-sequelize-starter!");
+});
+
+
+// Router for search feature
+app.use('/search', searchRouter);
+
+// Catch unhandled requests and forward to error handler.
+app.use((req, res, next) => {
+  const err = new Error("The requested resource couldn't be found.");
+  err.status = 404;
+  next(err);
+});
+
+// Custom error handlers.
+
+// Generic error handler.
+app.use((err, req, res, next) => {
+  // console.log('error -------------------------------')
+  res.status(err.status || 500);
+  const isProduction = environment === "production";
+  res.json({
+    title: err.title || "Server Error",
+    message: err.message,
+    errors: err.errors,
+    stack: isProduction ? null : err.stack,
+  });
+});
+
+module.exports = app
---
+++ routes/search.js
@@ -0,0 +1,63 @@
+const express = require("express");
+const router = express.Router();
+const { User, List, Task } = require("../db/models");
+const { asyncHandler } = require('../utils');
+const { Op } = require("sequelize");
+
+router.get('/:searchStr(\\w+)', asyncHandler(async (req, res) => {
+  let testArr = [
+    "hello",
+    "this",
+    "is a",
+    "test"
+  ]
+  
+  // const matchStr = req.params.searchStr.match(/^[\w\-]+[^\?]+$/);
+  // console.log("MATCH STRING!!!!!:   ", matchStr);
+  // console.log('URL CHECK: ', req.originalUrl);
+  console.log('URL CHECK: ',req.params.searchStr);
+
+  // const { searchString } = req.body;
+    const searchString = req.params.searchStr;
+
+  // Find matching tasks ======================================================
+  const matchingTasks = await Task.findAll({
+    where: {
+      taskName: {
+        [Op.iLike]: `%${searchString}%`,
+      }
+    }
+  });
+
+  // Find matching lists ======================================================
+  const matchingLists = await List.findAll({
+    where: {
+      listName: {
+        [Op.iLike]: `%${searchString}%`,
+      }
+    }
+  });
+
+
+  if(matchingTasks.length === 0) {
+    console.log("No Matches Found For TASKS");
+  } else {
+    matchingTasks.forEach(task => {
+      console.log(`MATCHING TASKS: ${task.taskName}`);
+    })
+  }
+  
+  if (matchingLists.length === 0) {
+    console.log("No Matches Found For LISTS");
+  } else {
+    matchingLists.forEach(list => {
+      console.log(`MATCHING LISTS: ${list.listName}`);
+    })
+  }
+  // res.send(`RESULTS: ${matchingTasks[0].taskName}`);
+  // console.log("FIRST TASK: ", searchMatchList[0].taskName);
+  res.send('DONE')
+
+}));
+
+module.exports = router;
