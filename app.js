a36a8cd40b68336ad7f40f5dc35e319da361f07f
TJ Taylor <tjtaylorjr@gmail.com>
2020-10-06 15:20:57 -0400

    added signup and login routes for user on backend, updated app.js and completed auth.js

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 //external requires
 const express = require('express');
 const morgan = require('morgan');
+const { ValidationError, HostNotFoundError } = require("sequelize");
 const path = require('path');
 
 
 //internal requires
-//const usersRouter = require("./routes/users");
+const usersRouter = require("./routes/users");
 
 
 const app = express();
@@ -14,17 +15,41 @@ app.set('view engine', 'pug');
 
 //external use statements
 app.use(morgan('dev'));
-app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
 
 //internal use statements
 app.use('/public', express.static(path.join(__dirname, 'public')));
-//app.use('/users', usersRouter);
+app.use('/users', usersRouter);
+
 
 //TODO: error handlers
+// unhandled requests
+  app.use((req, res, next) => {
+    const error = new Error("Resource could not be found.");
+    error.errors = ["Resource could not be found."];
+    error.status = 404;
+    next(error);
+  });
 
+//sequelize errors
+app.use((error, req, res, next) => {
+  if(error instanceof ValidationError) {
+    error.errors = error.errors.map((err) => err.message);
+    error.title = "Sequelize Error";
+  };
+  next(error);
+});
 
-
-
-
+//general error handler
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  const isProduction = environment === "production";
+  res.json({
+    title: error.title || "Server Error",
+    errors: error.errors,
+    stack: isProduction ? null : error.stack
+  });
+});
 
 module.exports = app;
---
+++ routes/users.js
@@ -0,0 +1,103 @@
+const express = require("express");
+const bycrpt = require("bcryptjs");
+const { check } = require("express-validator");
+const { asyncHandler, handleValidationErrors } = require("../utils/utils");
+const { getUserToken } = require("../utils/auth");
+const { User } = require("../db/models");
+
+const router = express.Router();
+
+const signupValidations = [
+  check("userName")
+    .exists({ checkFalsy: true })
+    .withMessage("A username is required.")
+    .isLength({ min: 3, max: 50 })
+    .withMessage("A username must be between 3 and 50 characters."),
+  check("firstName")
+    .exists({ checkFalsy: true })
+    .withMessage("A first name is required.")
+    .isLength({ min: 1, max: 50 })
+    .withMessage("A first name must be between 1 and 50 characters."),
+  check("lastName")
+    .exists({ checkFalsy: true })
+    .withMessage("A last name is required.")
+    .isLength({ min: 1, max: 50 })
+    .withMessage("A last name must be between 1 and 50 characters."),
+  check("confirmPassword")
+    .exists({ checkFalsy: true })
+    .withMessage("Please confirm your password")
+    .custom((value, { req }) => value === req.body.password)
+    .withMessage("Confirm Password field does not match password."),
+];
+
+const sharedAuthValidations = [
+  check("email")
+    .exists({ checkFalsy: true })
+    .isEmail()
+    .withMessage("A valid email address is required")
+    .isLength({ max: 100 })
+    .withMessage("Email address must be less than 100 characters"),
+  check("password")
+    .exists({ checkFalsy: true })
+    .withMessage("User password is required"),
+];
+
+//sign up
+router.post("/",
+  sharedAuthValidations,
+  signupValidations,
+  handleValidationErrors,
+  asyncHandler(async(req, res) => {
+    const {
+      userName,
+      firstName,
+      lastName,
+      email,
+      password,
+    } = req.body;
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = await User.create(
+      {
+       firstName,
+       lastName,
+       userName,
+       email,
+       hashedPassword
+      }
+    );
+
+    const token = await getUserToken(user);
+
+    res.status(201).json({
+      user: { id: user.id },
+      token
+    });
+  })
+);
+
+
+// log in
+router.post("/token", sharedAuthValidations,
+  asyncHandler(async(req, res, next) => {
+    const { email, password } = req.body;
+    const user = await User.findOne(
+      {
+        where: { email }
+      }
+    );
+    if (!user || !user.validatePassword(password)) {
+      const error = new Error("Invalid credentials");
+      error.status = 401;
+      error.title = "Invalid credentials";
+      error.errors = ["Unable to authenticate provided information. Please check user name and/or password."];
+      return next(error);
+    }
+
+    const token = getUserToken(user);
+    res.json({ token, user: { id: user.id }});
+  })
+);
+
+
+module.exports = router;
